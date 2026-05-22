f02ad9a4bf2605518a01526394662359db2e319e
BleckHall <34749786+BleckHall@users.noreply.github.com>
2026-05-22 03:15:06 +0200

    Add files via upload

---
+++ lang/de/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentifizierung
+    |--------------------------------------------------------------------------
+    |
+    | Diese Texte werden bei der Anmeldung und Authentifizierung angezeigt.
+    |
+    */
+
+    'failed' => 'Die eingegebenen Zugangsdaten konnten nicht gefunden werden.',
+    'password' => 'Das eingegebene Passwort ist falsch.',
+    'throttle' => 'Zu viele Anmeldeversuche. Bitte versuche es in :seconds Sekunden erneut.',
+
+];
---
+++ lang/de/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Passwort zurücksetzen
+    |--------------------------------------------------------------------------
+    |
+    | Diese Texte werden beim Zurücksetzen von Passwörtern angezeigt.
+    |
+    */
+
+    'reset' => 'Dein Passwort wurde erfolgreich zurückgesetzt.',
+    'sent' => 'Wir haben dir einen Link zum Zurücksetzen des Passworts per E-Mail gesendet.',
+    'throttled' => 'Bitte warte einen Moment, bevor du es erneut versuchst.',
+    'token' => 'Dieser Link zum Zurücksetzen des Passworts ist ungültig oder abgelaufen.',
+    'user' => 'Wir konnten keinen Benutzer mit dieser E-Mail-Adresse finden.',
+
+];
